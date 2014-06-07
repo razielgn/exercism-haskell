2043867f2c46a11e3039ab8a23ea2518223b03b4
Federico Ravasio <ravasio.federico@gmail.com>
2014-06-07 10:36:33 +0200

    Added grade-school.

---
+++ grade-school/School.hs
@@ -0,0 +1,22 @@
+module School (School, add, empty, sorted, grade) where
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.List (sort)
+import Data.Maybe (fromMaybe)
+
+type Name = String
+type Grade = Int
+type School = Map Grade [Name]
+
+add :: Grade -> Name -> School -> School
+add g name = Map.insertWith (++) g [name]
+
+empty :: School
+empty = Map.empty
+
+sorted :: School -> [(Grade, [Name])]
+sorted = Map.toAscList . fmap sort
+
+grade :: Grade -> School -> [Name]
+grade g m = fromMaybe [] $ Map.lookup g m
