d1aec744a0875919c7764c7af24d835c343848f5
Federico Ravasio <ravasio.federico@gmail.com>
2014-06-07 08:28:14 +0200

    Improved word-count via nitpicks.

--- word-count/WordCount.hs
+++ word-count/WordCount.hs
@@ -1,12 +1,10 @@
 module WordCount (wordCount) where
 
-import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, fromListWith)
 import Data.Char (isAlphaNum, toLower)
-import Data.List (foldl')
 
-wordCount :: String -> Map.Map String Int
-wordCount = count Map.empty . words . lowercase . transmute
-  where count = foldl' (flip insertion)
-        insertion w = Map.insertWith (+) w 1
+wordCount :: String -> Map String Int
+wordCount = fromListWith (+) . toList  . words . lowercase . transmute
+  where toList = map (\x -> (x, 1))
         lowercase = map toLower
         transmute = map (\x -> if isAlphaNum x then x else ' ')
