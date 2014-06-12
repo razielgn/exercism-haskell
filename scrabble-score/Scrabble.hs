02436bce5ae5f492ed61005dd2299fa788049619
Federico Ravasio <ravasio.federico@gmail.com>
2014-06-12 17:16:57 +0200

    Improved scrabble-score via nitpicks.

--- scrabble-score/Scrabble.hs
+++ scrabble-score/Scrabble.hs
@@ -4,20 +4,38 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Char (toUpper)
 
-scoreLetter :: Char -> Integer
-scoreLetter l = first $ Map.filter (elem l') scores
+scoreLetter :: Char -> Int
+scoreLetter l = Map.findWithDefault 0 l' scores
   where l' = toUpper l
-        first = head . Map.keys
 
-scoreWord :: String -> Integer
+scoreWord :: String -> Int
 scoreWord = sum . map scoreLetter
 
-scores :: Map Integer String
-scores = Map.fromList [(1,  "AEIOULNRST")
-                      ,(2,  "DG")
-                      ,(3,  "BCMP")
-                      ,(4,  "FHVWY")
-                      ,(5,  "K")
-                      ,(8,  "JX")
-                      ,(10, "QZ")
+scores :: Map Char Int
+scores = Map.fromList [('A', 1)
+                      ,('B', 3)
+                      ,('C', 3)
+                      ,('D', 2)
+                      ,('E', 1)
+                      ,('F', 4)
+                      ,('G', 2)
+                      ,('H', 4)
+                      ,('I', 1)
+                      ,('J', 8)
+                      ,('K', 5)
+                      ,('L', 1)
+                      ,('M', 3)
+                      ,('N', 1)
+                      ,('O', 1)
+                      ,('P', 3)
+                      ,('Q', 10)
+                      ,('R', 1)
+                      ,('S', 1)
+                      ,('T', 1)
+                      ,('U', 1)
+                      ,('V', 4)
+                      ,('W', 4)
+                      ,('X', 8)
+                      ,('Y', 4)
+                      ,('Z', 10)
                       ]
