3b54b789b944f746776490f149dd6d67377d8037
Federico Ravasio <ravasio.federico@gmail.com>
2014-06-13 00:59:19 +0200

    Improved binary via nitpicks.

--- binary/Binary.hs
+++ binary/Binary.hs
@@ -1,13 +1,13 @@
 module Binary (toDecimal) where
 
 import Data.Char (digitToInt)
-import qualified Data.Sequence as Seq
-import Data.Sequence (foldrWithIndex)
+import Data.List (foldl')
 
 toDecimal :: String -> Int
 toDecimal n
   | not valid = 0
   | otherwise = toDecimal' n
   where valid = all (`elem` "01") n
-        toDecimal' = foldrWithIndex folder 0 . Seq.fromList . reverse
-        folder i d = (+) (2 ^ i * digitToInt d)
+        toDecimal' = foldl' mapper 0 . zip indexes . reverse
+        indexes = [0..] :: [Int]
+        mapper acc (i, d) = acc + 2 ^ i * digitToInt d
