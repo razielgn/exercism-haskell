519c16cb3771ee931dc7592561ae176b37789e5f
Federico Ravasio <ravasio.federico@gmail.com>
2014-06-14 21:46:01 +0200

    Improved prime-factors via nitpicks.

--- prime-factors/PrimeFactors.hs
+++ prime-factors/PrimeFactors.hs
@@ -5,6 +5,8 @@ import Data.List (unfoldr)
 primeFactors :: Integer -> [Integer]
 primeFactors x = unfoldr unfolder (x, 2)
   where unfolder (1, _) = Nothing
-        unfolder (n, d) = if n `mod` d == 0
-                          then Just (d, (n `div` d, 2))
-                          else unfolder (n, succ d)
+        unfolder (n, d)
+          | stop n d       = Just (n, (1, d))
+          | n `mod` d == 0 = Just (d, (n `div` d, 2))
+          | otherwise      = unfolder (n, succ d)
+        stop n d = fromIntegral d > sqrt (fromIntegral n)
