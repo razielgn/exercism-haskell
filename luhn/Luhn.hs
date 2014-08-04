711786995054e6a92d189b78bbf838b2aaf891ec
Federico Ravasio <ravasio.federico@gmail.com>
2014-08-04 16:33:41 +0200

    Refactored luhn.

--- luhn/Luhn.hs
+++ luhn/Luhn.hs
@@ -7,23 +7,23 @@ module Luhn (
 , create
 ) where
 
+import Data.Char (digitToInt)
+
 checkDigit :: Integer -> Integer
 checkDigit = (`mod` 10)
 
 digits :: Integer -> [Integer]
-digits 0 = []
-digits n = let (div_, mod_) = divMod n 10
-           in digits div_ ++ [mod_]
+digits = map (toInteger . digitToInt) . show
 
 addends :: Integer -> [Integer]
 addends = double . digits
   where double xs = zipWith fn (range xs) xs
         range xs  = enumFrom $ if even $ length xs then 0 else 1 :: Integer
         fn i n
-          | even i    = let n' = n * 2
-                        in if n' > 9
-                           then n' - 9
-                           else n'
+          | even i = let n' = n * 2
+                     in if n' > 9
+                        then n' - 9
+                        else n'
           | otherwise = n
 
 checksum :: Integer -> Integer
@@ -33,9 +33,9 @@ isValid :: Integer -> Bool
 isValid = (== 0) . checksum
 
 create :: Integer -> Integer
-create n = let padded = n * 10
-               sum'   = checksum padded
-               digit  = if sum' == 0
-                        then sum'
-                        else 10 - sum'
-           in padded + digit
+create n = padded + digit
+  where padded = n * 10
+        sum'   = checksum padded
+        digit  = if sum' == 0
+                 then sum'
+                 else 10 - sum'
