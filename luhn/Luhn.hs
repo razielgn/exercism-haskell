9dcd7bb1a2ec02762c808b2052a32c7f671f1223
Federico Ravasio <ravasio.federico@gmail.com>
2014-08-05 15:32:48 +0200

    Refactored luhn.

--- luhn/Luhn.hs
+++ luhn/Luhn.hs
@@ -20,11 +20,9 @@ addends = double . digits
   where double xs = zipWith fn (range xs) xs
         range xs  = enumFrom $ if even $ length xs then 0 else 1 :: Integer
         fn i n
-          | even i = let n' = n * 2
-                     in if n' > 9
-                        then n' - 9
-                        else n'
+          | even i    = if n' > 9 then n' - 9 else n'
           | otherwise = n
+          where n' = n * 2
 
 checksum :: Integer -> Integer
 checksum = checkDigit . sum . addends
@@ -36,6 +34,6 @@ create :: Integer -> Integer
 create n = padded + digit
   where padded = n * 10
         sum'   = checksum padded
-        digit  = if sum' == 0
-                 then sum'
-                 else 10 - sum'
+        digit
+          | sum' == 0 = sum'
+          | otherwise = 10 - sum'
