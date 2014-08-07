93616c5874c822915dcf6cea2560bb3db39dbd02
Federico Ravasio <ravasio.federico@gmail.com>
2014-08-07 17:07:36 +0200

    Refactored matrix to use just a Vector.

--- matrix/Matrix.hs
+++ matrix/Matrix.hs
@@ -12,59 +12,52 @@ module Matrix (
 , fromList
 ) where
 
+import Control.Arrow ((&&&))
 import Data.Char (isSpace)
 import Data.List.Split (split, startsWith)
-import Data.Maybe (fromMaybe)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 
-newtype Matrix a = Matrix (Vector (Vector a))
-                   deriving (Eq, Show)
+data Matrix a = Matrix Int Int (Vector a)
+                deriving (Eq, Show)
 
 row :: Int -> Matrix a -> Vector a
-row i (Matrix m) = m V.! i
+row i (Matrix _ w v) = V.slice (w * i) w v
 
 column :: Int -> Matrix a -> Vector a
-column i (Matrix m) = V.map (V.! i) m
+column i (Matrix h w v) = V.map (v V.!) range
+  where range = V.enumFromStepN i w h
 
 rows :: Matrix a -> Int
-rows (Matrix m) = V.length m
+rows (Matrix h _ _) = h
 
 cols :: Matrix a -> Int
-cols (Matrix m) = V.length firstRow
-  where firstRow = fromMaybe V.empty $ m V.!? 0
+cols (Matrix _ w _) = w
 
 shape :: Matrix a -> (Int, Int)
-shape m = (rows m, cols m)
+shape = rows &&& cols
 
 transpose :: Matrix a -> Matrix a
-transpose (Matrix m) = Matrix $ go m
-  where go vs
-          | V.null vs       = V.empty
-          | V.null firstRow = go vss
-          | otherwise       = x `V.cons` V.map V.head vss `V.cons` go (xs `V.cons` V.map V.tail vss)
-          where vss      = V.tail vs
-                firstRow = V.head vs
-                x        = V.head firstRow
-                xs       = V.tail firstRow
+transpose m@(Matrix h w _) = Matrix w h transposed
+  where transposed = V.concatMap (`column` m) range
+        range      = V.enumFromN 0 w
 
 reshape :: (Int, Int) -> Matrix a -> Matrix a
-reshape (_, c) (Matrix m) = Matrix $ V.concatMap splitter m
-  where splitter v = V.fromList [v1, v2]
-          where (v1, v2) = V.splitAt c v
+reshape (h, w) (Matrix _ _ v) = Matrix h w v
 
 flatten :: Matrix a -> Vector a
-flatten (Matrix m) = V.foldl' (V.++) V.empty m
+flatten (Matrix _ _ v) = v
 
 fromString :: Read a => String -> Matrix a
-fromString = Matrix . createMatrix
-  where createMatrix = V.fromList . map createRow . lines
-        createRow    = V.fromList . map read . words'
-        splitQuoted  = map rstrip . split (startsWith " \"")
+fromString = fromList . map (map read . words') . lines
+  where splitQuoted  = map rstrip . split (startsWith " \"")
         rstrip       = dropWhile isSpace
         words' xs
           | '"' `elem` xs = splitQuoted xs
           | otherwise     = words xs
 
 fromList :: [[a]] -> Matrix a
-fromList = Matrix . V.fromList . map V.fromList
+fromList l = Matrix height' width' vector'
+  where height' = length l
+        width'  = if null l then 0 else length $ head l
+        vector' = V.fromList $ concat l
